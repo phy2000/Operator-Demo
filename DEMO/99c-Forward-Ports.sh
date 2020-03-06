9bdfd44c72345cc1689c9e83f628ffbf7bbfaafe
Paul Young <pyoung@confluent.io>
2020-03-06 10:21:11 -0500

    Print instructions to connect to forwarded ports

--- DEMO/99c-Forward-Ports.sh
+++ DEMO/99c-Forward-Ports.sh
@@ -2,3 +2,9 @@
 POD_NAME=$(kubectl get pods --namespace operator -l "app.kubernetes.io/instance=pyoung-grafana,app.kubernetes.io/name=grafana" -o jsonpath="{.items[0].metadata.name}")
 kubectl --namespace operator port-forward $POD_NAME 3000 &  
 kubectl -n operator port-forward controlcenter-0 12345:9021 &
+
+echo connect to control center at "localhost:12345"
+echo use admin/Developer1 to login
+
+echo connect to grafana at "localhost:3000"
+echo login with user "admin" and the password saved in "grafana-passwd.txt"
